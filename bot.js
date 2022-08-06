279d1b49ed8874cd96692a6c0001c54bcf85e5ca
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2022-08-06 07:05:21 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -91,7 +91,7 @@ emeraldawal: 5,
 umpanawal: 5,
 potionawal: 1
 }
-global.thumb = fs.readFileSync('./Zimbot/drips.jpg')
+global.thumb = fs.readFileSync('./drips.jpg')
 global.visoka = { url: 'https://i.imgur.com/LbmTdhf.mp4' }
 global.vid = { url: 'https://i.imgur.com/LbmTdhf.mp4' }
 
